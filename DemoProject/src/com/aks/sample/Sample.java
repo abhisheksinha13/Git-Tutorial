b1d9bc451ed7f447b38ebeff6e0233e1be035f09
abhisheksinha13 <abhisheksinha2014@gmail.com>
2021-04-04 23:20:24 +0530

    New commit -2

--- DemoProject/src/com/aks/sample/Sample.java
+++ DemoProject/src/com/aks/sample/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello");
-		System.out.println("Sample");
+		System.out.println("Demo");
 	}
 
 }
